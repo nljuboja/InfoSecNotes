3eb33a5e43a6de0690daadd314bb3022f33c5720
nljuboja <nljuboja@users.noreply.github.com>
2021-01-25 20:18:28 -0800

    Update ubuntu_start_script.sh

--- ubuntu_start_script.sh
+++ ubuntu_start_script.sh
@@ -1,3 +1,7 @@
 sudo apt update
 
-sudo apt install vim exuberant-ctags cscope tmux radare2 radare2-cutter
+sudo apt install vim exuberant-ctags cscope tmux radare2 radare2-cutter net-tools git python cmake build-essential
+
+r2pm init
+r2pm update
+r2pm -ci r2ghidra
