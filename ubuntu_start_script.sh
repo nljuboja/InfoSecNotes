f9acce008d77342a12a12fa4f09310c13e2585fb
nljuboja <nljuboja@users.noreply.github.com>
2021-01-29 07:21:54 -0800

    Update ubuntu_start_script.sh

--- ubuntu_start_script.sh
+++ ubuntu_start_script.sh
@@ -1,6 +1,14 @@
-sudo apt update
+sudo apt update -y
 
-sudo apt install vim exuberant-ctags cscope tmux radare2 radare2-cutter net-tools git python cmake build-essential gitk
+sudo apt install -y vim exuberant-ctags cscope tmux net-tools git python cmake build-essential gitk qttools5-dev-tools make
+
+mkdir ~/bin
+cd ~/bin
+git clone https://github.com/radareorg/radare2
+cd radare2 ; sys/install.sh
+
+cd ..
+wget https://github.com/radareorg/r2cutter/releases/download/v1.12.0/Cutter-v1.12.0-x64.Linux.AppImage
 
 r2pm init
 r2pm update
